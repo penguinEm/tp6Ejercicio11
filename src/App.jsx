b701997da21e031da43a0828236ea51a4a3689e2
emir paradi <emir.alejandro.paradi@gmail.com>
2024-01-31 15:03:42 -0300

    Template limpio para trabajar el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Container } from "react-bootstrap";
+import Titulo from "./components/Titulo";
+
+function App() {
+  return (
+    <>
+      <Container className="main my-5">
+        <Titulo></Titulo>
+      </Container>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,9 @@
+const Titulo = () => {
+  return (
+    <section className="text-center border-bottom">
+      <h1>Noticias</h1>
+    </section>
+  );
+};
+
+export default Titulo;
